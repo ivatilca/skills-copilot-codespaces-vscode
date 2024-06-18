2c80327ae9d4079aee500c6cd1ce6f1af09f3165
ivanatilca <ivanatilca@hotmail.com>
2024-06-18 15:03:08 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var skills = {
+        html: "HTML",
+        css: "CSS",
+        js: "JavaScript",
+        php: "PHP",
+        mysql: "MySQL",
+        nodejs: "Node.js",
+        react: "React",
+        vue: "Vue",
+        angular: "Angular"
+    };
+    console.log(skills);
+}
